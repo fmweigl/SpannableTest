85ecfd92b6ca28e02cda42d118a5a451b82f0a40
ext-florian.weigl <ext-florian.weigl@onefootball.com>
2020-07-09 18:40:43 +0200

    1st

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ImageSpan
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageSpan = ImageSpan(this, R.drawable.star)
+        val builder = SpannableStringBuilder(" ")
+        builder.setSpan(
+            imageSpan,
+            0,
+            1,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        Snackbar.make(findViewById(R.id.container), "Hello Snackbar", Snackbar.LENGTH_INDEFINITE)
+            .setAction(builder) {}.show()
+    }
+}
